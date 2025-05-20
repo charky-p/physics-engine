dbabb08770966a618c362a50bca7dec85f4e6d45
Charles Podesta <z5588335@ad.unsw.edu.au>
2025-05-20 20:37:14 +1000

    Initial example, generatin ideas

---
+++ examples/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "../src/Engine.cpp"
+
+int main() {
+    // Example usage
+    Engine engine();
+
+    // TODO - prototype
+    // engine.setGravity(9.8f);
+    // engine.createCircle(position 2D vector, radius, ...)
+    // engine.createSquare(position 2D vector, length, ...)
+    // some way of setting a "floor"
+
+    /*
+     _____
+    |     |
+    |     |
+    |_____|
+
+       ***
+     *     *
+    *       *
+    *       *
+     *     *
+       ***
+
+        Floor:
+    ---------------------------------------------
+    */
+}
